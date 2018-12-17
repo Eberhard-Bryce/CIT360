c1bf4e8d8a5ea3fc5194c240c4b37b1b48d8e396
Eberhard-Bryce <33768407+Eberhard-Bryce@users.noreply.github.com>
2018-12-16 22:28:51 -0700

    Delete RockResult.java

--- App Control Pattern/RockResult.java
+++
@@ -1,8 +0,0 @@
-package ACP;
-
-public class RockResult {
-	public void show(){
-		System.out.println("A player who decides to play rock will beat another player "
-				+ "who has chosen scissors, but will lose to one who has played paper");
-	}
-}
