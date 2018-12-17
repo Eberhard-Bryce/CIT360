f0327db1814a02b12efe9cd6450f0550aa341902
Eberhard-Bryce <33768407+Eberhard-Bryce@users.noreply.github.com>
2018-12-16 22:28:59 -0700

    Delete ScissorsResult.java

--- App Control Pattern/ScissorsResult.java
+++
@@ -1,8 +0,0 @@
-package ACP;
-
-public class ScissorsResult {
-	public void show(){
-		System.out.println("A player who decides to play scissors will beat another player "
-				+ "who has chosen paper, but will lose to one who has played rock");
-	}
-}
